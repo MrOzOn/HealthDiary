e0891f0eba93e9f5c66612d29c6ec3b3c232c1f6
MrOzOn <andrew_1982>
2020-08-17 23:48:07 +0300

    add Timber

--- app/src/main/java/com/mrozon/healthdiary/App.kt
+++ app/src/main/java/com/mrozon/healthdiary/App.kt
@@ -1,13 +1,19 @@
 package com.mrozon.healthdiary
 
-import android.app.Activity
 import android.app.Application
+import timber.log.Timber
 
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initTimber()
         initDagger()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
     }
 
     private fun initDagger() {
